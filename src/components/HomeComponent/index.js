9205569fb7ddc95961ec22e88126b1c2ad0e2266
aryloves <arylovessway@gmail.com>
2024-11-24 21:26:40 +0500

    Initial commit

--- src/components/HomeComponent/index.js
+++ src/components/HomeComponent/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import NewTweet from "./NewTweet";
 import TweetsList from "./TweetsList";
-import { KZ_IMG_PATH, NETFLIX_IMG_PATH} from './images';
+import { KZ_IMG_PATH, NETFLIX_IMG_PATH } from './images';
 
 class Home extends React.Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
             count: 0,
             content: '',
+            searchQuery: '', // Added state for search input
             tweets: [
                 {
                     id: 0,
-                    authorName: 'Netflix Türkiye ',
+                    authorName: 'Netflix Türkiye',
                     authorUsername: '@netflixtürkiye',
                     img: NETFLIX_IMG_PATH,
                     content: 'Güneşin doğmasına 3 saat kaldı, sezonu bitirmek zorunda değilsin. #YakamozS245',
@@ -34,8 +35,8 @@ class Home extends React.Component {
                     topic: 'education'
                 },
                 {
-                    id:2,
-                    authorName: 'Netflix Türkiye ',
+                    id: 2,
+                    authorName: 'Netflix Türkiye',
                     authorUsername: '@netflixtürkiye',
                     img: NETFLIX_IMG_PATH,
                     content: 'Güneşin doğmasına 3 saat kaldı, sezonu bitirmek zorunda değilsin. #YakamozS245',
@@ -43,51 +44,34 @@ class Home extends React.Component {
                     retweets: 127,
                     likes: 2.533,
                     topic: 'education'
-                },
+                }
             ],
-            filteredTweets: [
-                {
-                id: 0,
-                authorName: 'Netflix Türkiye',
-                authorUsername: '@netflixtürkiye',
-                img: NETFLIX_IMG_PATH,
-                content: 'Güneşin doğmasına 3 saat kaldı, sezonu bitirmek zorunda değilsin. #YakamozS245',
-                replies: 197,
-                retweets: 127,
-                likes: 2.533,
-                topic: 'politics'
-            },
-            {
-                id: 1,
-                authorName: 'Netflix Türkiye',
-                authorUsername: '@netflixtürkiye',
-                img: NETFLIX_IMG_PATH,
-                content: 'Güneşin doğmasına 3 saat kaldı, sezonu bitirmek zorunda değilsin. #YakamozS245',
-                replies: 197,
-                retweets: 127,
-                likes: 2.533,
-                topic: 'education'
-            },
-            {
-                id:2,
-                authorName: 'Netflix Türkiye',
-                authorUsername: '@netflixtürkiye',
-                img: NETFLIX_IMG_PATH,
-                content: 'Güneşin doğmasına 3 saat kaldı, sezonu bitirmek zorunda değilsin. #YakamozS245',
-                replies: 197,
-                retweets: 127,
-                likes: 2.533,
-                topic: 'education'
-            }
-        ]
-        }
+            filteredTweets: [] // Initial filtered tweets are empty
+        };
+    }
+
+    componentDidMount() {
+        // Initialize filtered tweets with all tweets
+        this.setState({ filteredTweets: this.state.tweets });
     }
 
     onChangeTextInput = (e) => {
         this.setState({
             content: e.target.value
-        })
-    }
+        });
+    };
+
+    onSearchInputChange = (e) => {
+        const searchQuery = e.target.value.toLowerCase();
+        this.setState({
+            searchQuery,
+            filteredTweets: this.state.tweets.filter(tweet =>
+                tweet.content.toLowerCase().includes(searchQuery) ||
+                tweet.authorName.toLowerCase().includes(searchQuery) ||
+                tweet.topic.toLowerCase().includes(searchQuery)
+            )
+        });
+    };
 
     addToTweets = () => {
         const newTweet = {
@@ -99,38 +83,46 @@ class Home extends React.Component {
             replies: 0,
             retweets: 0,
             likes: 0,
-            topic: 'shi'
-        }
+            topic: 'general'
+        };
 
         this.setState({
-            tweets: [...this.state.tweets, newTweet ],
+            tweets: [...this.state.tweets, newTweet],
+            filteredTweets: [...this.state.tweets, newTweet], // Update filtered tweets
             content: ''
-        })
-    }
+        });
+    };
 
     deleteTweet = (id) => {
+        const updatedTweets = this.state.tweets.filter(item => item.id !== id);
         this.setState({
-            tweets: this.state.tweets.filter(item=>item.id!==id)
-        })
-    }
+            tweets: updatedTweets,
+            filteredTweets: updatedTweets.filter(tweet =>
+                tweet.content.toLowerCase().includes(this.state.searchQuery) ||
+                tweet.authorName.toLowerCase().includes(this.state.searchQuery) ||
+                tweet.topic.toLowerCase().includes(this.state.searchQuery)
+            )
+        });
+    };
 
-    filterTweetsByTopic = (topic) => {
-        console.log('filter', topic)
-        this.setState({
-            filteredTweets: this.state.tweets.filter(item=>item.topic === topic)
-        })
-    }
- 
-    render(){
-        const { tweets, content } = this.state;
+    render() {
+        const { content, searchQuery, filteredTweets } = this.state;
 
-        return(
+        return (
             <div className="w-50 mt-3">
                 <h5 className="mx-3">Home</h5>
-                <NewTweet content={content} onChangeTextInput={this.onChangeTextInput} onTweet={this.addToTweets}/>
-                <TweetsList tweets={tweets} deleteTweet={this.deleteTweet}/>
+                {/* Search Input */}
+                <input
+                    type="text"
+                    placeholder="Search tweets..."
+                    value={searchQuery}
+                    onChange={this.onSearchInputChange}
+                    className="form-control my-3"
+                />
+                <NewTweet content={content} onChangeTextInput={this.onChangeTextInput} onTweet={this.addToTweets} />
+                <TweetsList tweets={filteredTweets} deleteTweet={this.deleteTweet} />
             </div>
-        )
+        );
     }
 }
 
